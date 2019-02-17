b2364a3b19a203aaf6dc42120d78ad919e152e30
jdollar <dollarhidejoel@gmail.com>
2019-02-16 21:38:52 -0500

    Add a successful test to mp4upload and increased documentation on the process

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Plugin.video.animepie
 
 Unofficial kodi addon to play videos from [animepie](http://animepie.to)
+
+# Installing for local development
+
+In order to run the unit tests you will need to install the python libraries in the requirements.txt file
+
+```shell
+pip install -r requirements.txt
+```
+
+NOTE: For unit tests we are installing js2py. When running on kodi it is going to be using the version
+from my repository. Need to ensure that those two stay in sync with each other
+
+# Running unit tests
+
+To actually run the tests we will use pytest
+
+```
+pytest -v
+```
+
+If you want to view test coverage then you can use the pytest-cov package
+
+```shell
+pytest --cov=resources.lib tests/
+```
--- tests/resources/lib/embed_processors/test_mp4upload.py
+++ tests/resources/lib/embed_processors/test_mp4upload.py
@@ -1,35 +1,59 @@
 import sys
+import os
 import unittest
 from mock import MagicMock
+from bs4 import BeautifulSoup
 from resources.lib.animepie_exception import AnimePieException
 
 class TestMp4UploadProcessor(unittest.TestCase):
+    def generate_mock_addon_inst(self, localized_string, title):
+      mock_addon_inst = MagicMock()
+      mock_addon_inst.getLocalizedString = MagicMock(return_value=localized_string)
+      mock_addon_inst.getAddonInfo = MagicMock(return_value="title")
+      return mock_addon_inst
+
     def setUp(self):
+      self.dir_path = os.path.dirname(os.path.realpath(__file__))
+
       mock_addon = MagicMock()
 
-      self.mock_js2py = MagicMock()
       self.mock_addon = mock_addon
 
-      sys.modules['js2py'] = self.mock_js2py
       sys.modules['xbmcaddon'] = self.mock_addon
 
     def test_file_was_moved(self):
       localized_string = "test"
+      fixture_path = self.dir_path + "/fixtures/mp4upload/mp4upload_file_deleted.html"
 
-      mock_soup = MagicMock()
-      mock_soup.find = MagicMock(return_value=None)
+      with open(fixture_path, "r") as fixture:
+        mock_empty_response = fixture.read()
 
-      mock_addon_inst = MagicMock()
-      mock_addon_inst.getLocalizedString = MagicMock(return_value=localized_string)
-      mock_addon_inst.getAddonInfo = MagicMock(return_value="title")
+      soup = BeautifulSoup(mock_empty_response, 'html.parser')
+
+      mock_addon_inst = self.generate_mock_addon_inst(localized_string, "title")
       self.mock_addon.Addon = MagicMock(return_value=mock_addon_inst)
 
       from resources.lib.embed_processors import mp4upload
 
-      (err, src_url) = mp4upload.retrieve_source_url(mock_soup)
+      (err, src_url) = mp4upload.retrieve_source_url(soup)
       self.assertIsInstance(err, AnimePieException)
       self.assertEqual(err.args[0], localized_string)
       self.assertIsNone(src_url)
 
     def test_source_url_extraction(self):
-      self.assertEquals(False, True)
+      fixture_path = self.dir_path + "/fixtures/mp4upload/mp4upload_success.html"
+
+      with open(fixture_path, "r") as fixture:
+        mock_response = fixture.read()
+
+      soup = BeautifulSoup(mock_response, 'html.parser')
+
+      mock_addon_inst = self.generate_mock_addon_inst("test", "title")
+      self.mock_addon.Addon = MagicMock(return_value=mock_addon_inst)
+
+      from resources.lib.embed_processors import mp4upload
+
+      (err, src_url) = mp4upload.retrieve_source_url(soup)
+      self.assertIsNone(err)
+      self.assertEqual(src_url, "https://www4.mp4upload.com:282/d/rcxxbfluz3b4quuor6va2psvjj4duz7tloglcecimbapilmbtxryrb3t/video.mp4")
+
