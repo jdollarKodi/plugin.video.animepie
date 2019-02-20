a0331984194bb1c3f8cd8b7c97a30c7e82385141
jdollar <dollarhidejoel@gmail.com>
2019-02-20 18:37:18 -0500

    Add routes tests

---
+++ tests/resources/lib/routes/test_routes.py
@@ -0,0 +1,42 @@
+import unittest
+from mock import call, patch, MagicMock
+
+class TestRoutes(unittest.TestCase):
+    def setUp(self):
+        self.mock_index = MagicMock()
+        self.mock_anime_list = MagicMock()
+        self.mock_episode_list = MagicMock()
+        self.mock_video_sources = MagicMock()
+        self.mock_play_source = MagicMock()
+
+        modules = {
+            "resources.lib.routes.index": self.mock_index,
+            "resources.lib.routes.animelist": self.mock_anime_list,
+            "resources.lib.routes.episodelist": self.mock_episode_list,
+            "resources.lib.routes.videosources": self.mock_video_sources,
+            "resources.lib.routes.playsource": self.mock_play_source
+        }
+
+        self.module_patcher = patch.dict('sys.modules', modules)
+        self.module_patcher.start()
+
+    def tearDown(self):
+        self.module_patcher.stop()
+
+    def test_route_generation(self):
+        from resources.lib.routes.routes import generate_all_routes
+
+        mock_plugin = "test"
+
+        generate_all_routes(mock_plugin)
+
+        route_mocks = [
+            self.mock_index,
+            self.mock_anime_list,
+            self.mock_episode_list,
+            self.mock_video_sources,
+            self.mock_play_source,
+        ]
+
+        for route_mock in route_mocks:
+            route_mock.generate_routes.assert_called_once_with(mock_plugin)
